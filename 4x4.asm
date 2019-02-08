ed34f97b31b4560cadc23df0672c9e303176ea3a
jg3016 <jg3016@skbl-422-014.ph.ic.ac.uk>
2019-02-08 12:01:36 +0000

    cleaned up code and started subroutine for if statements (check functions)

--- 4x4.asm
+++ 4x4.asm
@@ -1,3 +1,26 @@
-; test commit
+#include p18f87k22.inc
 
+    global  keyboard_columns, OUTPUT_D, register_test
+
+acs0	udata_acs
+register_test	res 1
+
+keyboard    code
+
+keyboard_columns
+    banksel PADCFG1
+    bsf	    PADCFG1, REPU, BANKED
+    clrf    LATE
+    movlw   0x0f
+    movwf   TRISE
+    
+	
+OUTPUT_D
+	movlw	0x00
+	movwf	TRISD
+	movff	PORTE, register_test ; goto current line in code
+	movff	register_test, PORTD
+	return
+    
+    end
 
